e889f009141e9b7d1d8f316be50996bf053031c5
wjsdusdk <wjsdk1570@gmail.com>
2022-07-15 14:52:04 +0900

    220715

--- js/script.js
+++ js/script.js
@@ -1,13 +1,76 @@
 $(document).ready(function () {
+  // nav
+
+  var b = $("nav").offset().top;
+  // nav 처음위치, nav가 fixed되있어서 스크롤하면 위치가 바뀌기 때문에 스크롤 선언 밖에 적음
+
+  $(window).scroll(function () {
+    // nav 스크롤 효과
+    var a = $(this).scrollTop(); // 스크롤 위치
+    if (b < a) {
+      $("nav").addClass("on");
+    } else {
+      $("nav").removeClass("on");
+    }
+  });
+
   // 메뉴
 
   $(".menu_icon").click(function () {
     $(".menu_icon p:nth-child(1)").toggleClass("on");
     $(".menu_icon p:nth-child(2)").toggleClass("on");
     $(".menu_icon p:nth-child(3)").toggleClass("on");
-    $("#menu").toggleClass("on");
+    $(".menu").toggleClass("on");
     $(".search_icon p:nth-child(1)").toggleClass("on");
     $(".search_icon p:nth-child(2)").toggleClass("on");
+  });
+
+  // line 배너
+
+  $(".line_tab:nth-child(1)").click(function () {
+    $(".line_bn li:nth-child(1)").css("display", "block");
+    $(".line_bn li:nth-child(2)").css("display", "none");
+    $(".line_bn li:nth-child(3)").css("display", "none");
+  });
+  $(".line_tab:nth-child(2)").click(function () {
+    $(".line_bn li:nth-child(1)").css("display", "none");
+    $(".line_bn li:nth-child(2)").css("display", "block");
+    $(".line_bn li:nth-child(3)").css("display", "none");
+  });
+  $(".line_tab:nth-child(3)").click(function () {
+    $(".line_bn li:nth-child(1)").css("display", "none");
+    $(".line_bn li:nth-child(2)").css("display", "none");
+    $(".line_bn li:nth-child(3)").css("display", "block");
+  });
+
+  // line 탭
+
+  $(".line_tab").click(function () {
+    var tab_id = $(this).attr("data-tab");
+
+    $(".line_tab").removeClass("line_tab_current");
+    $(".line_tab-content").removeClass("line_tab-content_current");
+
+    $(this).addClass("line_tab_current");
+    $("#" + tab_id).addClass("line_tab-content_current");
+  });
+
+  // line 슬라이드
+
+  $(".line_tab li:nth-child(1)").click(function () {
+    $(".line_blemish").css("z-index", "12");
+    $(".line_intensive").css("z-index", "11");
+    $(".line_tightening").css("z-index", "11");
+  });
+  $(".line_tab li:nth-child(2)").click(function () {
+    $(".line_blemish").css("z-index", "11");
+    $(".line_intensive").css("z-index", "12");
+    $(".line_tightening").css("z-index", "11");
+  });
+  $(".line_tab li:nth-child(3)").click(function () {
+    $(".line_blemish").css("z-index", "11");
+    $(".line_intensive").css("z-index", "11");
+    $(".line_tightening").css("z-index", "12");
   });
 
   // 푸터 - 하위메뉴 화살표
@@ -37,7 +100,7 @@ docHeight = Math.max(docElem.scrollHeight, docElem.offsetHeight);
 // console.log(docHeight);
 
 if (docHeight != "undefined") {
-  offset = docHeight / 4;
+  offset = docHeight / 8;
 }
 
 window.addEventListener("scroll", function () {
--- js/swiper.js
+++ js/swiper.js
@@ -1,5 +1,6 @@
 const mb_swiper = new Swiper(".mb-section .swiper", {
   speed: 300, // 슬라이드 / 페이드 속도
+  slidesPerView: 1,
 
   // Effects
   effect: "fade", // 전환 효과 'slide' or 'fade' or 'cube' or 'coverflow' or 'flip'
@@ -23,26 +24,16 @@ const mb_swiper = new Swiper(".mb-section .swiper", {
     el: ".swiper-pagination",
     type: "progressbar", // 'bullets' or 'progressbar' or 'fraction' or 'custom'
   },
-
-  // Breakpoints
-  breakpoints: {
-    768: {
-      // when window width is >= 768px
-    },
-    992: {
-      // when window width is >= 1024px
-    },
-    1200: {
-      // when window width is >= 1200px
-    },
-    1400: {
-      // when window width is >= 1200px
-    },
-  }, // 반응형 슬라이드
 });
+
+
+
+
 
 const best_swiper = new Swiper(".best-section .swiper", {
   speed: 300, // 슬라이드 / 페이드 속도
+  slidesPerView: 1,
+  spaceBetween: 10,
 
   // Effects
   effect: "slide", // 전환 효과 'slide' or 'fade' or 'cube' or 'coverflow' or 'flip'
@@ -51,6 +42,62 @@ const best_swiper = new Swiper(".best-section .swiper", {
 
   // Clicks
   slideToClickedSlide: false,
+
+  // loop
+  loop: true, // 무한반복
+
+  // Cursor
+  grabCursor: false,
+
+  // navigation (7183)
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // pagination (7715)
+  pagination: {
+    el: ".swiper-pagination",
+    bulletElement: "span",
+    clickable: true, // 바 클릭 가능 여부
+    hideOnClick: false,
+    type: "bullets", // 'bullets' or 'progressbar' or 'fraction' or 'custom'
+    dynamicBullets: true,
+    dynamicMainBullets: 1,
+  },
+
+  // Breakpoints
+  breakpoints: {
+    768: {
+      // when window width is >= 768px
+      slidesPerView: 2,
+    },
+    992: {
+      // when window width is >= 1024px
+      slidesPerView: 3,
+    },
+    1200: {
+      // when window width is >= 1200px
+      slidesPerView: 3,
+    },
+    1400: {
+      // when window width is >= 1400px
+      slidesPerView: 4,
+    },
+  }, // 반응형 슬라이드
+});
+
+
+
+
+
+const line_swiper = new Swiper(".line-section .swiper", {
+  speed: 300, // 슬라이드 / 페이드 속도
+  slidesPerView: 2,
+  spaceBetween: 0,
+
+  // Effects
+  effect: "slide", // 전환 효과 'slide' or 'fade' or 'cube' or 'coverflow' or 'flip'
 
   // loop
   loop: true, // 무한반복
@@ -76,36 +123,32 @@ const best_swiper = new Swiper(".best-section .swiper", {
   breakpoints: {
     768: {
       // when window width is >= 768px
-      slidesPerView: 2,
-      spaceBetween: 10,
     },
     992: {
       // when window width is >= 1024px
-      slidesPerView: 3,
-      spaceBetween: 10,
     },
     1200: {
       // when window width is >= 1200px
-      slidesPerView: 4,
-      spaceBetween: 10,
     },
     1400: {
       // when window width is >= 1400px
-      slidesPerView: 4,
-      spaceBetween: 10,
     },
   }, // 반응형 슬라이드
 });
 
+
+
+
+
 const sns_swiper = new Swiper(".sns-section .swiper", {
   speed: 300, // 슬라이드 / 페이드 속도
+  slidesPerView: 2, // 한 화면에 보여질 슬라이드 갯수 "auto"
+  spaceBetween: 10, // 슬라이드 사이 간격
 
   // Effects
   effect: "slide", // 전환 효과 'slide' or 'fade' or 'cube' or 'coverflow' or 'flip'
 
   // Slides grid
-  spaceBetween: 0, // 슬라이드 사이 간격
-  slidesPerView: 1, // 한 화면에 보여질 슬라이드 갯수 "auto"
   slidesPerColumn: 1,
   slidesPerColumnFill: "column",
 
